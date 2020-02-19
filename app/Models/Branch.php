98b78b2090cb77fca5731652f426e38a4a093a33
ojan <ojanherdika@gmail.com>
2020-02-19 13:59:19 +0700

    models udah

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -8,7 +8,7 @@ class Branch extends Model
 {
     protected $primaryKey='id_branch';
     protected $table = 'branch';
-    protected $fillable = ['id_branch','nama','telepon','alamat','website','email','created_by','created_time','id_karyawan'];
+    protected $fillable = ['id_branch','id_company','uniq_code','lokasi','latitude','longitude','created_by','created_time','id_karyawan'];
     public $incrementing = false;
     public $timestamps = false;
 }
